683fc7f8a35bde84bc92c6666b8f15507ff9a470
russiaaiyappa <russia.ayya@gmail.com>
2018-07-18 16:42:27 -0400

    added CourseRepository

---
+++ src/main/java/com/example/webdevsummer22018serverjavarabp/repositories/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.webdevsummer22018serverjavarabp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdevsummer22018serverjavarabp.models.Course;
+
+public interface CourseRepository extends CrudRepository<Course, Integer> {
+
+}
