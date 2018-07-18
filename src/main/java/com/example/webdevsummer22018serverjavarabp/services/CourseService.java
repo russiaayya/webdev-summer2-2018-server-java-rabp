0bdb424e997bdc258e3b0b4a8adff52c170803fb
russiaaiyappa <russia.ayya@gmail.com>
2018-07-18 16:48:38 -0400

    added CourseService.java

---
+++ src/main/java/com/example/webdevsummer22018serverjavarabp/services/CourseService.java
@@ -0,0 +1,39 @@
+package com.example.webdevsummer22018serverjavarabp.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.webdevsummer22018serverjavarabp.models.Course;
+import com.example.webdevsummer22018serverjavarabp.repositories.CourseRepository;
+
+@RestController
+@CrossOrigin(origins="*")
+public class CourseService {
+	@Autowired
+	CourseRepository courseRepository;
+	
+	@GetMapping("/api/course")
+	public List<Course> findAllCourses() {
+		return (List<Course>) courseRepository.findAll();
+	}
+	
+	@PostMapping("/api/course")
+	public Course createCourse(
+			@RequestBody Course course) {
+		return courseRepository.save(course);
+	}
+	
+	@DeleteMapping("/api/course/{courseId}")
+	public void deleteCourse(@PathVariable("courseId") int id) {
+		
+		courseRepository.deleteById(id);
+	}
+}
