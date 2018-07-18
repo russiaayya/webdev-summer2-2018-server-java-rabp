24eb5b309df3d33ec655474f0a2022706168f4eb
russiaaiyappa <russia.ayya@gmail.com>
2018-07-18 17:30:43 -0400

    added module and lesson repostories

---
+++ src/main/java/com/example/webdevsummer22018serverjavarabp/repositories/LessonRepository.java
@@ -0,0 +1,9 @@
+package com.example.webdevsummer22018serverjavarabp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdevsummer22018serverjavarabp.models.Lesson;
+
+public interface LessonRepository extends CrudRepository<Lesson, Integer> {
+
+}
---
+++ src/main/java/com/example/webdevsummer22018serverjavarabp/repositories/ModuleRepository.java
@@ -0,0 +1,9 @@
+package com.example.webdevsummer22018serverjavarabp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdevsummer22018serverjavarabp.models.Module;
+
+public interface ModuleRepository extends CrudRepository<Module, Integer>{
+
+}
