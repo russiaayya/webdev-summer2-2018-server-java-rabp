fa14e18de6354f88bd81af27038e47e938fc924f
russiaaiyappa <russia.ayya@gmail.com>
2018-07-30 10:59:42 -0400

    modidied widget and topic models

--- src/main/java/com/example/webdevsummer22018serverjavarabp/models/Widget.java
+++ src/main/java/com/example/webdevsummer22018serverjavarabp/models/Widget.java
@@ -1,52 +1,58 @@
 package com.example.webdevsummer22018serverjavarabp.models;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+enum ListType {
+	ordered,
+	unordered
+}
 
 @Entity
 public class Widget {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
-	private String title;
-	private String widgetType;
-	// List Widget
-	private String listItems;
-	private boolean ordered;
-	// Heading Widget
+	private String name;
+	private int widgetOrder;
 	private String text;
+	private String className;
+	private String style;
+	private String width;
+	private String height;
 	private int size;
+	private String href;
+	private String src;
+	private String listItems;
+	@Enumerated(EnumType.STRING)
+	private ListType listType;
+	@ManyToOne
+	@JsonIgnore
+	private Topic topic;
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getTitle() {
-		return title;
+	public String getName() {
+		return name;
 	}
-	public void setTitle(String title) {
-		this.title = title;
+	public void setName(String name) {
+		this.name = name;
 	}
-	public String getWidgetType() {
-		return widgetType;
+	public int getWidgetOrder() {
+		return widgetOrder;
 	}
-	public void setWidgetType(String widgetType) {
-		this.widgetType = widgetType;
-	}
-	public String getListItems() {
-		return listItems;
-	}
-	public void setListItems(String listItems) {
-		this.listItems = listItems;
-	}
-	public boolean isOrdered() {
-		return ordered;
-	}
-	public void setOrdered(boolean ordered) {
-		this.ordered = ordered;
+	public void setWidgetOrder(int widgetOrder) {
+		this.widgetOrder = widgetOrder;
 	}
 	public String getText() {
 		return text;
@@ -54,11 +60,77 @@ public class Widget {
 	public void setText(String text) {
 		this.text = text;
 	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public String getStyle() {
+		return style;
+	}
+	public void setStyle(String style) {
+		this.style = style;
+	}
+	public String getWidth() {
+		return width;
+	}
+	public void setWidth(String width) {
+		this.width = width;
+	}
+	public String getHeight() {
+		return height;
+	}
+	public void setHeight(String height) {
+		this.height = height;
+	}
 	public int getSize() {
 		return size;
 	}
 	public void setSize(int size) {
 		this.size = size;
 	}
+	public String getHref() {
+		return href;
+	}
+	public void setHref(String href) {
+		this.href = href;
+	}
+	public String getSrc() {
+		return src;
+	}
+	public void setSrc(String src) {
+		this.src = src;
+	}
+	public String getListItems() {
+		return listItems;
+	}
+	public void setListItems(String listItems) {
+		this.listItems = listItems;
+	}
+	public ListType getListType() {
+		return listType;
+	}
+	public void setListType(ListType listType) {
+		this.listType = listType;
+	}
+	public Topic getTopic() {
+		return topic;
+	}
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
+	
+	
+//	private String widgetType;
+//	// List Widget
+//	private String listItems;
+//	private boolean ordered;
+//	// Heading Widget
+//	private String text;
+//	private int size;
+	
+	
+	
 	
 }
